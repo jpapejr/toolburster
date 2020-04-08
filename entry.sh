412668532c1937c0584e2ab4126e928df7cb95b0
John Pape <jtpape@us.ibm.com>
2020-04-08 14:01:33 -0400

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -3,8 +3,13 @@ set -x
 
 cd /utils/cloudshell-toolbox
 git pull origin master
+git checkout -- *
+chmod ugo+rwx . 
 
 cd ../vsix-cache
 git pull origin master
+git checkout -- *
+chmod ugo+rwx . 
+
 
 tail -f /dev/null
