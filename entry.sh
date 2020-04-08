087beb2d4ee4bef2843d0ed86ed49494cf0fe8a0
John Pape <jtpape@us.ibm.com>
2020-04-08 13:34:32 -0400

    Update entry.sh

--- entry.sh
+++ entry.sh
@@ -2,15 +2,9 @@
 set -x
 
 cd /utils/cloudshell-toolbox
-git init
-git remote add origin https://github.com/jpapejr/cloudshell-toolbox
-git fetch
-git reset origin/master  # Required when the versioned files existed in path before "git init" of this repo.
-git checkout -t origin/master
+git pull origin master
 
 cd ../vsix-cache
-git init
-git remote add origin https://github.com/jpapejr/vsix-cache
-git fetch
-git reset origin/master  # Required when the versioned files existed in path before "git init" of this repo.
-git checkout -t origin/master
+git pull origin master
+
+tail -f /dev/null
